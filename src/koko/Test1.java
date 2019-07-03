0ebbf0f7ae93508aa0d30fe3cee463f77795939d
krogot88 <51113908+krogot88@users.noreply.github.com>
2019-07-03 15:33:40 +0400

    commit to multithreading

--- src/koko/Test1.java
+++ src/koko/Test1.java
@@ -2,19 +2,38 @@ package koko;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Test1 {
 	private static int cycles = 1_000_000;
-	private static List<String> listForPool = new ArrayList<String>();
-	private static List<String> listForHeap = new ArrayList<String>();
 
-	public static void main(String[] arg) throws InterruptedException {			
-		System.out.println("pool time " + poolString());
-		System.out.println("heap time " + heapString());
-
+	public static void main(String[] arg) throws ExecutionException, InterruptedException {
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		Callable<Long> poolTest = new PoolTest(cycles);
+		Callable<Long> heapTest = new HeapTest(cycles);
+		Future<Long> timePoolTest = executorService.submit(poolTest);
+		Future<Long> timeHeaptest = executorService.submit(heapTest);
+		System.out.println("time poolTest: " + timePoolTest.get());
+		System.out.println("time heapTest: " + timeHeaptest.get());
 	}
 
-	public static Long poolString() {	
+}
+
+class PoolTest implements Callable<Long> {
+	private final int cycles;
+	private static List<String> listForPool = new ArrayList<String>();
+
+	public PoolTest(int cycles) {
+		super();
+		this.cycles = cycles;
+	}
+
+	@Override
+	public Long call() throws Exception {
 		String s = null;
 		Long time = 0L;
 		for (int i = 0; i < cycles; i++) {
@@ -27,20 +46,29 @@ public class Test1 {
 		}
 		return time;
 	}
+}
 
-	public static Long heapString() {		
+class HeapTest implements Callable<Long> {
+	private final int cycles;
+	private static List<String> listForHeap = new ArrayList<String>();	
+
+	public HeapTest(int cycles) {
+		super();
+		this.cycles = cycles;
+	}
+
+	@Override
+	public Long call() throws Exception {
 		String s = null;
 		Long time = 0L;
 		for (int i = 0; i < cycles; i++) {
-			String temp = Integer.toString(i);
+			String temp = Integer.toString(i);			
 			long timeStart = System.currentTimeMillis();
-			s = new String(temp);
-			long timeEnd = System.currentTimeMillis();
+			s = new String(temp); 
+			long timeEnd = System.currentTimeMillis();			
 			time = time + (timeEnd - timeStart);
 			listForHeap.add(s);
 		}
 		return time;
 	}
 }
-
-
